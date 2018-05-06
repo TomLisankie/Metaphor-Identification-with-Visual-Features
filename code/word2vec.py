6e118c1c560fa22ad45af3a5011a941dfa153e7b
Thomas Lisankie <thomas.lisankie@protonmail.com>
2018-05-06 12:26:54 -0400

    -

--- code/word2vec.py
+++ code/word2vec.py
@@ -10,7 +10,7 @@ sentence_list = []
 for sentence in unpacker:
     sentence_list.append(sentence[1])
 
-word2vec_model = Word2Vec(sentences=sentence_list, sg=1, size=100, window=5, min_count=100, negative=10)
+word2vec_model = Word2Vec(sentences=sentence_list, sg=1, size=100, window=5, min_count=100, negative=10, workers=32)
 
 word2vec_model.train(sentence_list, total_examples=len(sentence_list), epochs=3)
 
